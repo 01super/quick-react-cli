c888eea3c4176bd010828273e1acbcd5978d5948
01super <fk1024@outlook.com>
2020-09-19 11:37:37 +0800

    refactor: 调整目录结构

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const ora = require("ora");
-const download = require("download-git-repo");
+var inquirer = require("inquirer");
+var fs = require("fs");
+var ora = require("ora");
+var download = require("download-git-repo");
 
-const questions = [
+var questions = [
   {
     type: "input", // type为答题的类型
     name: "projectName", // 本题的key，待会获取answers时通过这个key获取value
@@ -40,23 +40,23 @@ const questions = [
 
 inquirer.prompt(questions).then((answers) => {
   // 获取答案
-  const version = answers.version;
-  const projectName = answers.projectName;
-  const repository = answers.repository;
+  var version = answers.version;
+  var projectName = answers.projectName;
+  var repository = answers.repository;
 
-  const spinner = ora("downloading...");
+  var spinner = ora("downloading...");
   spinner.color = "green";
   spinner.start();
 
-  const editFile = function () {
+  var editFile = function () {
     // 读取文件
     fs.readFile(`${process.cwd()}/${projectName}/package.json`, (err, data) => {
       if (err) throw err;
       // 获取json数据并修改项目名称和版本号
-      let _data = JSON.parse(data.toString());
+      var _data = JSON.parse(data.toString());
       _data.name = projectName;
       _data.version = version;
-      let str = JSON.stringify(_data, null, 4);
+      var str = JSON.stringify(_data, null, 4);
       // 写入文件
       fs.writeFile(
         `${process.cwd()}/${projectName}/package.json`,
